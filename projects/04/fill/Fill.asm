69d8a77ad13499ebd5f9d1b06bf1ee876338d2f1
Nabe Hsieh <nabe_hsieh@ms.usi.com.tw>
2016-07-06 17:44:23 +0800

    Finish all tasks on project 04.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,52 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+(MONITOR)
+	// set pos to be SCREEN
+	@SCREEN
+	D=A
+	@pos
+	M=D
+
+	@KBD
+	// if no key has been preesed, then goto WHITE
+	D=M
+	@WHITE
+	D;JEQ
+	// else goto BLACK
+	@BLACK
+	0;JMP
+
+(BLACK)
+	@val
+	M=-1 // All set be 1, so it is black
+	@LOOP
+	0;JMP
+(WHITE)
+	@val
+	M=0 // All set be 0, so it is white
+	@LOOP
+	0;JMP
+
+(LOOP)
+	@SCREEN
+	D=A
+	@pos
+	D=M-D
+	// if pos is 8192, go back to MONITOR(SCREEN size is 8k word)
+	@8192
+	D=D-A
+	@MONITOR
+	D;JEQ
+
+	// set val to RAM[pos]
+	@val
+	D=M
+	@pos
+	A=M
+	M=D
+
+	@pos
+	M=M+1
+	@LOOP
+	0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	// Set R2 to be zero
+	@R2
+	M=0
+	// Check the R0 and R1 is zero or not, if yes, goto ZERO
+	@R0
+	D=M
+	@ZERO
+	D;JEQ
+	@R1
+	D=M
+	@ZERO
+	D;JEQ
+
+	// use R0 as an increment, R1 as times and R2 to store sum 
+(LOOP)
+	// if ( R1 == 0 ) goto END
+	@R1
+	D=M
+	@END
+	D;JEQ
+	// R2 = R2 + R0
+	@R0
+	D=M
+	@R2
+	M=D+M
+	// R1--
+	@R1
+	M=M-1
+	@LOOP
+	0;JMP
+
+(ZERO)
+	@R2
+	M=0
+
+(END)
+	@END
+	0;JMP
